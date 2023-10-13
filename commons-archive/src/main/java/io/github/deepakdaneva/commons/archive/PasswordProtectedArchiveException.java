14ce0ca654acdfa598e7ef26df209339c9d9f989
Deepak Kumar Jangir <68489729+deepakdaneva@users.noreply.github.com>
2023-10-13 14:41:09 +0530

    This Merge Includes #6
    1. Added and configured the `spotless` maven plugin to format files during compilation.
    2. Added a `license-header` file for replacing it in each java file during compilation by the spotless maven plugin.
    3. Added `eclipse-formatter.xml` formatting rules.

--- commons-archive/src/main/java/io/github/deepakdaneva/commons/archive/PasswordProtectedArchiveException.java
+++ commons-archive/src/main/java/io/github/deepakdaneva/commons/archive/PasswordProtectedArchiveException.java
@@ -1,3 +1,15 @@
+/*
+ * Copyright (C) 2023 Deepak Kumar Jangir
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
 package io.github.deepakdaneva.commons.archive;
 
 /**
@@ -6,6 +18,12 @@ package io.github.deepakdaneva.commons.archive;
  * @since 1
  */
 public class PasswordProtectedArchiveException extends RuntimeException {
+    /**
+     * Constructor with exception message and throwable instance as a argument
+     * 
+     * @param message message of the exception
+     * @param throwable throwable instance
+     */
     public PasswordProtectedArchiveException(String message, Throwable throwable) {
         super(message, throwable);
     }
--- commons-archive/src/test/java/io/github/deepakdaneva/commons/archive/TestArchiveUtil.java
+++ commons-archive/src/test/java/io/github/deepakdaneva/commons/archive/TestArchiveUtil.java
@@ -1,16 +1,27 @@
+/*
+ * Copyright (C) 2023 Deepak Kumar Jangir
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
 package io.github.deepakdaneva.commons.archive;
 
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import org.apache.commons.compress.archivers.ArchiveException;
 import org.apache.commons.compress.archivers.ArchiveInputStream;
 import org.apache.commons.compress.compressors.CompressorException;
 import org.apache.commons.io.IOUtils;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
-import java.io.BufferedInputStream;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
 
 /**
  * @author Deepak Kumar Jangir
@@ -19,6 +30,13 @@ import java.io.InputStream;
  */
 public class TestArchiveUtil {
 
+    /**
+     * Test {@code 'application/zip'} file
+     * 
+     * @throws IOException io excepion
+     * @throws CompressorException compress exception
+     * @throws ArchiveException archive exception
+     */
     @Test
     public void testZip() throws IOException, CompressorException, ArchiveException {
         InputStream is = new BufferedInputStream(new FileInputStream(TestConstants.TEST_FILES_PATH.resolve("testfiles/txt_file_zip").toString()));
@@ -27,6 +45,13 @@ public class TestArchiveUtil {
         IOUtils.closeQuietly(ais, is);
     }
 
+    /**
+     * Test {@code 'application/gzip'} file
+     * 
+     * @throws IOException io excepion
+     * @throws CompressorException compress exception
+     * @throws ArchiveException archive exception
+     */
     @Test
     public void testTar() throws CompressorException, IOException, ArchiveException {
         InputStream is = new BufferedInputStream(new FileInputStream(TestConstants.TEST_FILES_PATH.resolve("testfiles/txt_file_tar_gz").toString()));
@@ -35,6 +60,13 @@ public class TestArchiveUtil {
         IOUtils.closeQuietly(ais, is);
     }
 
+    /**
+     * Test {@code 'application/x-7z-compressed'} file
+     * 
+     * @throws IOException io excepion
+     * @throws CompressorException compress exception
+     * @throws ArchiveException archive exception
+     */
     @Test
     public void test7Zip() throws IOException, CompressorException, ArchiveException {
         InputStream is = new BufferedInputStream(new FileInputStream(TestConstants.TEST_FILES_PATH.resolve("testfiles/txt_file_7z").toString()));
@@ -43,6 +75,13 @@ public class TestArchiveUtil {
         IOUtils.closeQuietly(ais, is);
     }
 
+    /**
+     * Test {@code 'text/plain'} file with wrong {@code 'application/zip'} mime type
+     * 
+     * @throws IOException io excepion
+     * @throws CompressorException compress exception
+     * @throws ArchiveException archive exception
+     */
     @Test
     public void testNonZip() throws IOException, CompressorException, ArchiveException {
         InputStream is = new BufferedInputStream(new FileInputStream(TestConstants.TEST_FILES_PATH.resolve("testfiles/txt_file").toString()));
@@ -52,6 +91,13 @@ public class TestArchiveUtil {
         IOUtils.closeQuietly(is);
     }
 
+    /**
+     * Test {@code 'text/plain'} file with wrong {@code 'application/x-7z-compressed'} mime type
+     * 
+     * @throws IOException io excepion
+     * @throws CompressorException compress exception
+     * @throws ArchiveException archive exception
+     */
     @Test
     public void testNon7Zip() throws IOException, CompressorException, ArchiveException {
         InputStream is = new BufferedInputStream(new FileInputStream(TestConstants.TEST_FILES_PATH.resolve("testfiles/txt_file").toString()));
@@ -61,6 +107,13 @@ public class TestArchiveUtil {
         IOUtils.closeQuietly(is);
     }
 
+    /**
+     * Test {@code 'text/plain'} file with wrong {@code 'application/gzip'} mime type
+     * 
+     * @throws IOException io excepion
+     * @throws CompressorException compress exception
+     * @throws ArchiveException archive exception
+     */
     @Test
     public void testNonTar() throws CompressorException, IOException, ArchiveException {
         InputStream is = new BufferedInputStream(new FileInputStream(TestConstants.TEST_FILES_PATH.resolve("testfiles/txt_file").toString()));
